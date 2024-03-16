c33480cb00e8b8f13ee334e1db095777d9fc87e6
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-16 17:46:17 -0300

    [FEAT: #26] - Soma o total de pagamentos feitos para um emprestimo.
    close #26

--- app/core/services.py
+++ app/core/services.py
@@ -1,4 +1,27 @@
-def extract_client_id(meta):
+import uuid
+from decimal import Decimal
+
+from django.db.models import Sum
+
+from app.core.models import Loan
+
+ZERO = Decimal("0.00")
+
+
+class TotalPaymentLoanNotFound(Exception):
+    def __init__(self, uuid):
+        super().__init__(self, f'Emprestimo com o "{uuid}" não existe.')
+
+
+def extract_client_id(meta: dict) -> str:
+    """Obtem o IP do cliente.
+
+    Args:
+        meta (dict): Meta dados do resquest do django
+
+    Returns:
+        str: o ipv4 ou ipv6 do cliente.
+    """
 
     if x_forwared_for := meta.get("HTTP_X_FORWARDED_FOR"):
         ip = x_forwared_for.split(",")[0].strip()
@@ -6,3 +29,24 @@ def extract_client_id(meta):
         ip = meta.get("REMOTE_ADDR")
 
     return ip
+
+
+def total_payment_for_the_loan(uuid: uuid) -> Decimal:
+    """Calcula o total já pago para um determinado emprestimo
+
+    Args:
+        uuid (uuid): Identificador unico do emprestimo
+
+    Raises:
+        TotalPaymentLoanNotFound: Retorna um erro caso o emprestimo não seja achado.
+
+    Returns:
+        Decimal: Soma do valor total pago.
+    """
+
+    try:
+        loan = Loan.objects.get(uuid=uuid)
+    except Loan.DoesNotExist as e:
+        raise TotalPaymentLoanNotFound(uuid) from e
+
+    return loan.payments.aggregate(total=Sum("value", default=ZERO))["total"]
--- app/core/tests/test_services.py
+++ app/core/tests/test_services.py
@@ -1,8 +1,12 @@
+from decimal import Decimal
+from uuid import uuid4
+
 import pytest
 from django.test import RequestFactory
 
 from app.conftest import fake
-from app.core.services import extract_client_id
+from app.core.models import Payment
+from app.core.services import TotalPaymentLoanNotFound, extract_client_id, total_payment_for_the_loan
 
 
 @pytest.mark.unity()
@@ -23,3 +27,37 @@ def test_positive_extract_client_id_with_x_forwared_for(ips):
     expected_client_ip = ips.split(",")[0]
 
     assert extract_client_id(request.META) == expected_client_ip
+
+
+@pytest.mark.integration()
+def test_positive_loan_payments_sum(loan):
+
+    Payment.objects.bulk_create(
+        [
+            Payment(loan=loan, value=200.49),
+            Payment(loan=loan, value=299.51),
+            Payment(loan=loan, value=1000.00),
+        ]
+    )
+
+    total = total_payment_for_the_loan(loan.uuid)
+
+    assert total == Decimal(1500.00)
+
+
+@pytest.mark.integration()
+def test_negative_loan_payments_sum_without_any_payments(loan):
+
+    total = total_payment_for_the_loan(loan.uuid)
+
+    assert total == Decimal(0.00)
+
+
+@pytest.mark.integration()
+def test_negative_loan_payments_sum_loan_not_exists(db):
+
+    uuid = uuid4()
+    msg = f'Emprestimo com o "{uuid}" não existe.'
+
+    with pytest.raises(TotalPaymentLoanNotFound, match=msg):
+        total_payment_for_the_loan(uuid)
