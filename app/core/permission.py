bd5864b61d1a823cbd4abb9952b04a0f7ec6cf3c
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-16 02:26:54 -0300

    [FEAT: #24] - Le um pagamento por ID.
    close #24

--- app/core/permission.py
+++ app/core/permission.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission
 
 
-class OnlyUserCanAccessOwnLoan(BasePermission):
+class UserOnlyCanAccessOwnLoan(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user.pk == request.user.pk
+
+
+class UserOnlyCanAccessOwnPayment(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.loan.user.pk == request.user.pk
---
+++ app/core/tests/views/payments/test_retrieve.py
@@ -0,0 +1,64 @@
+import pytest
+from django.shortcuts import resolve_url
+from rest_framework import status
+
+END_POINT_NAME = "core:payment-retrieve"
+
+
+@pytest.mark.integration()
+def test_positive(client_api_auth, payment):
+
+    url = resolve_url(END_POINT_NAME, payment.id)
+
+    response = client_api_auth.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    body = response.json()
+
+    assert body["value"] == str(payment.value)
+    assert body["loan"] == str(payment.loan.uuid)
+    assert body["created_at"] == payment.created_at.astimezone().isoformat()
+    assert body["modified_at"] == payment.modified_at.astimezone().isoformat()
+
+
+@pytest.mark.integration()
+def test_negative_user_not_must_retrieve_other_user_payment(client_api_auth, other_user_payment):
+
+    url = resolve_url(END_POINT_NAME, other_user_payment.id)
+
+    response = client_api_auth.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    assert response.json() == {"detail": "Você não tem permissão para executar essa ação."}
+
+
+@pytest.mark.integration()
+def test_negative_wrong_token(client_api, db):
+
+    url = resolve_url(END_POINT_NAME, 1)
+
+    client_api.credentials(HTTP_AUTHORIZATION="Token invalid_token")
+
+    response = client_api.get(url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    body = response.json()
+
+    assert body == {"detail": "Token inválido."}
+
+
+@pytest.mark.integration()
+def test_negative_without_token(client_api):
+
+    url = resolve_url(END_POINT_NAME, 1)
+
+    response = client_api.get(url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    body = response.json()
+
+    assert body == {"detail": "As credenciais de autenticação não foram fornecidas."}
