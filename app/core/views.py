0b8e3f9010fa979ea4434d2ce9b30f543178bf7b
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-15 21:31:19 -0300

    [FEAT #15] - Cria o recurso para listar e criar pagamentos.
    close #15

--- app/core/views.py
+++ app/core/views.py
@@ -3,21 +3,12 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 
-from app import __version__
-from app.core.models import Loan
-from app.core.serializers import LoanSerializer
+from app.core.models import Loan, Payment
+from app.core.serializers import LoanSerializer, PaymentSerializer
 from app.core.services import extract_client_id
 
 
-@extend_schema(tags=["core"])
-@api_view(["GET"])
-@permission_classes([])
-def api_version(request):
-    """Mostra a versão da api"""
-    return Response({"version": __version__})
-
-
-@extend_schema(tags=["core"])
+@extend_schema(tags=["debug"])
 @api_view(["GET"])
 @permission_classes([])
 def get_client_ip(request):
@@ -35,8 +26,41 @@ class LoansLC(ListCreateAPIView):
 
     @extend_schema(summary="Lista os emprestimo")
     def get(self, request, *args, **kwargs):
-        """Retorna os emprestimos do usuario"""
+        """Retorna os emprestimos do usuario logado. O usuario é
+        obtido pelo Token.
+        """
         return super().get(request, *args, **kwargs)
+
+    @extend_schema(summary="Cria o emprestimo")
+    def post(self, request, *args, **kwargs):
+        """Cria o emprestimo do usuario auth. O usuario é
+        obtido pelo Token.
+        """
+        return super().post(request, *args, **kwargs)
+
+
+class PaymentLC(ListCreateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(loan__user=self.request.user)
+
+    @extend_schema(summary="Lista de pagamentos")
+    def get(self, request, *args, **kwargs):
+        """Retorna os pagamentos usuario. O usuario é
+        obtido pelo Token.
+        """
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(summary="Cria o pagamento para um emprestimo")
+    def post(self, request, *args, **kwargs):
+        """Cria o pagamento para emprestimo.O usuario é
+        obtido pelo Token.
+        """
+        return super().post(request, *args, **kwargs)
 
 
 loans_lc = LoansLC.as_view()
+payment_lc = PaymentLC.as_view()
